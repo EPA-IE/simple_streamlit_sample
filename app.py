a68bdc6ecf23a9bbdeec7688620ad3d1e11ddfc3
Claire <clairebyrne731@gmail.com>
2025-06-06 10:53:04 +0100

    simple app works locally

--- app.py
+++ app.py
@@ -7,11 +7,8 @@ import pandas as pd
 st.title('Simple test app')
 
 # generate data
-df = pd.DataFrame(np.random.randint(0,500,size=(50, 3)), columns=list('ABC')) 
+df = pd.DataFrame(np.random.randint(0,100,size=(50, 3)), columns=list('ABC')) 
 st.write(df)
-
-
-# df.plot(x='A', y='B', kind='scatter', s='C')
 
 fig = px.scatter(df, x='A', y='B', size='C')
 st.plotly_chart(fig)
